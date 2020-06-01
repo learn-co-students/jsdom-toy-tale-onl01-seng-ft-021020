fa4cd19f0f2c4de5b72baef0992cfee1f9bab386
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-06-01 15:25:10 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -13,3 +13,76 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+
+fetch('http://localhost:3000/toys/')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+      myJson.forEach((toy) => {
+        const newToy = new Toy(toy)
+        toyContainerToAppend.innerHTML += newToy.render()
+      })
+
+  })
+
+  toyForm.addEventListener("submit", function(event) {
+    event.preventDefault()
+      const userInputNewToyName = event.target.querySelector("#ToyNameId").value
+      const userInputNewToyURL = event.target.querySelector("#ToyImageId").value
+      const newToy = {
+        "name": userInputNewToyName,
+        "image": userInputNewToyURL,
+        "likes": 0
+      }
+
+    fetch('http://localhost:3000/toys', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify (
+        newToy
+      )
+    }).then(response => response.json())
+      .then(jsonToys => {
+        const newCreatedToy = new Toy(jsonToys)
+        toyContainerToAppend.innerHTML += newCreatedToy.render()
+
+    })
+    event.target.reset()
+  })
+
+
+  toyContainerToAppend.addEventListener("click", function(event) {
+      let buttonId = event.target.id
+      console.log(buttonId)
+
+      let findToy = Toy.find(buttonId)
+      console.log(findToy)
+      let likeCount = findToy.likes
+
+      fetch(`http://localhost:3000/toys/${buttonId}`, {
+          method: 'PATCH',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            likes: ++findToy.likes
+          })
+        }).then((response) => {
+          return response.json()
+        }).then((jsonObj) => {
+          Object.assign(findToy, jsonObj) //takes this first object and assigns it to the second argument
+          event.target.parentElement.parentElement.querySelector('p').innerText = `${findToy.likes} Likes`
+        })
+  })
+
+
+
+
+
+
